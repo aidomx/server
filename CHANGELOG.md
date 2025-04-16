1487a3a462fbbb3d88e7bec4174fd6ac004d199d
wadahkode <mvp.dedefilaras@gmail.com>
2025-04-16 06:45:22 +0000

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Aidomx Server changelog rilis version
 
-## v0.0.1
+## v0.0.1 - 16 April 2025
 
 ### Added
 
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  target: 'es2024',
+  target: 'esnext',
   outDir: 'dist',
   splitting: false,
   minify: true,
